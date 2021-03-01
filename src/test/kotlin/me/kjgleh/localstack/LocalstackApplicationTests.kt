1efdd5627c164092c321fd88b8b2ef9e2eced5b8
김정근 <gogoogjg@tripstore.kr>
2021-03-01 17:32:37 +0900

    initialize project

---
+++ src/test/kotlin/me/kjgleh/localstack/LocalstackApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.kjgleh.localstack
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LocalstackApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
